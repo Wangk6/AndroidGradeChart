0ae706611781f9647fe139cfb2d90a294b7ae37b
Kevin Wanggit config --global user.name Kevin <Wangk6@farmingdale.edu>
2019-11-21 22:16:29 -0500

    Finished Barchart

--- app/src/main/java/com/example/soblab/androidgradechart/BarChartActivity.java
+++ app/src/main/java/com/example/soblab/androidgradechart/BarChartActivity.java
@@ -1,5 +1,7 @@
 package com.example.soblab.androidgradechart;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -13,35 +15,50 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 import java.util.ArrayList;
 
 public class BarChartActivity extends AppCompatActivity {
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String AAVERAGE = "aAverage";
+    public static final String BAVERAGE = "bAverage";
+    public static final String CAVERAGE = "cAverage";
+    public static final String DAVERAGE = "dAverage";
+    public static final String FAVERAGE = "fAverage";
+    BarChart chart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_barchart);
-        BarChart chart = findViewById(R.id.barchart);
+        chart = findViewById(R.id.barchart);
+        createBarChart();
+    }
+
+    private void createBarChart(){
+
+
+        SharedPreferences grades = BarChartActivity.this.getSharedPreferences(
+                SHARED_PREFS, Context.MODE_PRIVATE);
 
         ArrayList<BarEntry> NoOfEmp = new ArrayList<>();
 
-        NoOfEmp.add(new BarEntry(945f, 0));
-        NoOfEmp.add(new BarEntry(1040f, 1));
-        NoOfEmp.add(new BarEntry(1133f, 2));
-        NoOfEmp.add(new BarEntry(1240f, 3));
-        NoOfEmp.add(new BarEntry(1369f, 4));
+        NoOfEmp.add(new BarEntry(grades.getFloat(AAVERAGE, 0.0f), 0));
+        NoOfEmp.add(new BarEntry(grades.getFloat(BAVERAGE, 0.0f), 1));
+        NoOfEmp.add(new BarEntry(grades.getFloat(CAVERAGE, 0.0f), 2));
+        NoOfEmp.add(new BarEntry(grades.getFloat(DAVERAGE, 0.0f), 3));
+        NoOfEmp.add(new BarEntry(grades.getFloat(FAVERAGE, 0.0f), 4));
 
         BarDataSet bardataset = new BarDataSet(NoOfEmp, "Average Grade");
-
         ArrayList<String> label = new ArrayList<>();
 
-        label.add("A");
-        label.add("B");
-        label.add("C");
-        label.add("D");
-        label.add("F");
+        label.add("A Avg");
+        label.add("B Avg");
+        label.add("C Avg");
+        label.add("D Avg");
+        label.add("F Avg");
 
         chart.animateY(5000);
-
+        chart.setDescription("");
+        chart.setDrawBarShadow(true);
         BarData data = new BarData(label, bardataset);
-
+        data.setValueTextSize(30f);
         bardataset.setColors(ColorTemplate.COLORFUL_COLORS);
         chart.setData(data);
     }
--- app/src/main/java/com/example/soblab/androidgradechart/MainActivity.java
+++ app/src/main/java/com/example/soblab/androidgradechart/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.soblab.androidgradechart;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,7 +14,12 @@ import com.github.mikephil.charting.charts.BarChart;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String AAVERAGE = "aAverage";
+    public static final String BAVERAGE = "bAverage";
+    public static final String CAVERAGE = "cAverage";
+    public static final String DAVERAGE = "dAverage";
+    public static final String FAVERAGE = "fAverage";
     EditText numOfStudents, aStudent, bStudent, cStudent, dStudent, fStudent;
     Button calculate;
     float aAverage, bAverage, cAverage, dAverage, fAverage;
@@ -33,59 +40,80 @@ public class MainActivity extends AppCompatActivity {
         calculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                calcPercentage();
 
-                //Alert Dialog
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setMessage("A Average: " + String.valueOf(aAverage) + "%\n" + "B Average: " + String.valueOf(bAverage) + "%\n"
-                        + "C Average: " +String.valueOf(cAverage) + "%\n"+ "D Average: " +String.valueOf(dAverage) +
-                        "%\n"+"F Average: " + String.valueOf(fAverage) + "%\n").setTitle("Your averages");
+                if(calcPercentage() == true) {
 
-                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        //Go to next fragment
-                        Intent I = new Intent(MainActivity.this, BarChartActivity.class);
-                        startActivity(I);
-                    }
-                });
-                builder.show();
+                    //Alert Dialog
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setMessage("A Average: " + String.valueOf(aAverage) + "%\n" + "B Average: " + String.valueOf(bAverage) + "%\n"
+                            + "C Average: " + String.valueOf(cAverage) + "%\n" + "D Average: " + String.valueOf(dAverage) +
+                            "%\n" + "F Average: " + String.valueOf(fAverage) + "%\n").setTitle("Your averages");
+
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            //Go to next fragment
+                            Intent I = new Intent(MainActivity.this, BarChartActivity.class);
+                            startActivity(I);
+                        }
+                    });
+                    builder.show();
+                }
             }
         });
     }
-    private void calcPercentage(){
-        float totalStudents = Float.parseFloat(numOfStudents.getText().toString());
-        aAverage = (Float.parseFloat(aStudent.getText().toString()) / totalStudents) * 100;
-        bAverage = (Float.parseFloat(bStudent.getText().toString()) / totalStudents) * 100;
-        cAverage = (Float.parseFloat(cStudent.getText().toString()) / totalStudents) * 100;
-        dAverage = (Float.parseFloat(dStudent.getText().toString()) / totalStudents) * 100;
-        fAverage = (Float.parseFloat(fStudent.getText().toString()) / totalStudents) * 100;
+    private boolean calcPercentage(){
+        if(!numOfStudents.getText().toString().equals("") && !aStudent.getText().toString().equals("")&&
+                !bStudent.getText().toString().equals("")&& !cStudent.getText().toString().equals("")&& !dStudent.getText().toString().equals("")
+                && !fStudent.getText().toString().equals("")) {
+            float totalStudents = Float.parseFloat(numOfStudents.getText().toString());
+            aAverage = (Float.parseFloat(aStudent.getText().toString()) / totalStudents) * 100;
+            bAverage = (Float.parseFloat(bStudent.getText().toString()) / totalStudents) * 100;
+            cAverage = (Float.parseFloat(cStudent.getText().toString()) / totalStudents) * 100;
+            dAverage = (Float.parseFloat(dStudent.getText().toString()) / totalStudents) * 100;
+            fAverage = (Float.parseFloat(fStudent.getText().toString()) / totalStudents) * 100;
 
-        int studentTotal = Integer.parseInt(numOfStudents.getText().toString());
-        if(studentTotal <= 0 && studentTotal >= 100){
-            //Display error
-            numOfStudents.setText("*");
-        }
+            //Average should be btn 0% - 100%
+            if (aAverage <= 0f && aAverage >= 100f) {
+                //Display error
+                aStudent.setText("*");
+                return false;
+            }
+            if (bAverage <= 0f && bAverage >= 100f) {
+                //Display error
+                bStudent.setText("*");
+                return false;
 
-        if(aAverage <= 0f && aAverage >= 100f){
-            //Display error
-            aStudent.setText("*");
+            }
+            if (cAverage <= 0f && cAverage >= 100f) {
+                //Display error
+                cStudent.setText("*");
+                return false;
+
+            }
+            if (dAverage <= 0f && dAverage >= 100f) {
+                //Display error
+                dStudent.setText("*");
+                return false;
+
+            }
+            if (fAverage <= 0f && fAverage >= 100f) {
+                //Display error
+                fStudent.setText("*");
+                return false;
+            }
+
+            SharedPreferences sharedPref = MainActivity.this.getSharedPreferences(
+                    SHARED_PREFS, Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putFloat(AAVERAGE, aAverage);
+            editor.putFloat(BAVERAGE, bAverage);
+            editor.putFloat(CAVERAGE, cAverage);
+            editor.putFloat(DAVERAGE, dAverage);
+            editor.putFloat(FAVERAGE, fAverage);
+            editor.commit();
+            return true; //Passes test
         }
-        if(bAverage <= 0f && bAverage >= 100f){
-            //Display error
-            bStudent.setText("*");
-        }
-        if(cAverage <= 0f && cAverage >= 100f){
-            //Display error
-            cStudent.setText("*");
-        }
-        if(dAverage <= 0f && dAverage >= 100f){
-            //Display error
-            dStudent.setText("*");
-        }
-        if(fAverage <= 0f && fAverage >= 100f){
-            //Display error
-            fStudent.setText("*");
-        }
+        return false;
     }
 }
